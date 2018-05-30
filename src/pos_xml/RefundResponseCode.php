0ecc2eccf3d6d87b72592f9366a2f86498228ae4
ryazanov <a.ryazanov@platron.ru>
2018-05-30 18:22:37 +0300

    fix bug

--- src/pos_xml/RefundResponseCode.php
+++ src/pos_xml/RefundResponseCode.php
@@ -3,11 +3,11 @@
 namespace Platron\multicarta\pos_xml;
 
 class RefundResponseCode extends ResponseCode {
-	REFUND_INVALID_TERMINAL_IDENTIFIER = '207'; // Неверный идентификатор терминала
-	REFUND_ORIGINAL_OPERATION_NOT_FOUND = '208'; // Оригинальная операция не найдена
-	REFUND_UNABLE_TO_PERFORM_AN_OPERATION = '209'; // Невозможно провести операцию
-	REFUND_SYSTEM_ERROR = '809'; // Системная ошибка
-	REFUND_INVALID_SESSION_NUMBER = '888'; // Неверный номер сессии
+	const REFUND_INVALID_TERMINAL_IDENTIFIER = '207'; // Неверный идентификатор терминала
+	const REFUND_ORIGINAL_OPERATION_NOT_FOUND = '208'; // Оригинальная операция не найдена
+	const REFUND_UNABLE_TO_PERFORM_AN_OPERATION = '209'; // Невозможно провести операцию
+	const REFUND_SYSTEM_ERROR = '809'; // Системная ошибка
+	const REFUND_INVALID_SESSION_NUMBER = '888'; // Неверный номер сессии
 
 	// TODO need to rename constant
 }
