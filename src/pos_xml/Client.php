ab81cb8493f26857b5360f27b08310d4ff6ae06b
ryazanov <a.ryazanov@platron.ru>
2018-06-25 11:23:30 +0300

    избавился от зависимости от сертификатов в клиенте

--- src/pos_xml/Client.php
+++ src/pos_xml/Client.php
@@ -9,16 +9,6 @@ class Client {
 	/**
 	 * @var string
 	 */
-	private $certificatePath;
-
-	/**
-	 * @var string
-	 */
-	private $privateKeyPath;
-
-	/**
-	 * @var string
-	 */
 	private $resultMessage;
 
 	/**
@@ -31,27 +21,24 @@ class Client {
 	 */
 	private $errorMessage;
 
-	public function __construct(
-		string $certificatePath,
-		string $privateKeyPath
-	) {
-		$this->certificatePath = $certificatePath;
-		$this->privateKeyPath = $privateKeyPath;
-	}
-
 	/**
 	 * @param string $url
 	 * @param array $request
 	 * @return SimpleXMLElement
 	 */
-	public function sendRequest(string $url, array $request) {
+	public function sendRequest(
+		string $url,
+		array $request,
+		string $certificatePath,
+		string $privateKeyPath
+	) {
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, $url.'?'.http_build_query($request));
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($curl, CURLOPT_SSLCERT, $this->getCertificatePath());
-		curl_setopt($curl, CURLOPT_SSLKEY, $this->getPrivateKeyPath());
+		curl_setopt($curl, CURLOPT_SSLCERT, $certificatePath);
+		curl_setopt($curl, CURLOPT_SSLKEY, $privateKeyPath);
 
 		$this->resultMessage = curl_exec($curl);
 		$this->errorCode = curl_errno($curl);
@@ -84,19 +71,5 @@ class Client {
 	 */
 	public function getErrorMessage() {
 		return $this->errorMessage;
-	}
-
-	/**
-	 * @return string
-	 */
-	protected function getCertificatePath() {
-		return $this->certificatePath;
-	}
-
-	/**
-	 * @return string
-	 */
-	protected function getPrivateKeyPath() {
-		return $this->privateKeyPath;
 	}
 }
