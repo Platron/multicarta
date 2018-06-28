4b811aef81bb908c90435fdf39a0a86df7ffface
ryazanov <a.ryazanov@platron.ru>
2018-06-28 15:11:25 +0300

    изменил интерфейс хелпера для рукуррентов

--- src/pos_xml/RecurringPaymentDataHelper.php
+++ src/pos_xml/RecurringPaymentDataHelper.php
@@ -4,123 +4,102 @@ namespace Platron\multicarta\pos_xml;
 
 use Platron\multicarta\PaymentSystemBrand;
 
-class RecurringDataHelper {
+class RecurringPaymentDataHelper {
 
 	const CONDITION = 0;
-	const NEED_RECURRING_DATA = 1;
-
-	/**
-	 * @var PaymentSystemBrand $paymentSystemBrand
-	 */
-	private $paymentSystemBrand;
-
-	/**
-	 * @var Mita $mita
-	 */
-	private $mita;
+	const NEED_ADDITIONAL_DATA = 1;
 
 	/**
 	 * @param PaymentSystemBrand $paymentSystemBrand
 	 * @param Mita $mita
+	 * @return string
 	 */
-	public function __construct(
+	public function getCondition(
 		PaymentSystemBrand $paymentSystemBrand,
 		Mita $mita
 	) {
-		$this->setPaymentSystemBrand($paymentSystemBrand);
-		$this->setMita($mita);
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getCondition() {
-		$dataSet = $this->getDataSet();
+		$dataSet = $this->getDataSet($paymentSystemBrand, $mita);
 		return $dataSet[self::CONDITION];
 	}
 
 	/**
+	 * @param PaymentSystemBrand $paymentSystemBrand
+	 * @param Mita $mita
 	 * @return boolean
 	 */
-	public function needRecurringData() {
-		$dataSet = $this->getDataSet();
-		return $dataSet[self::NEED_RECURRING_DATA];
+	public function needAdditionalData(
+		PaymentSystemBrand $paymentSystemBrand,
+		Mita $mita
+	) {
+		$dataSet = $this->getDataSet($paymentSystemBrand, $mita);
+		return $dataSet[self::NEED_ADDITIONAL_DATA];
 	}
 
 	/**
 	 * @param PaymentSystemBrand $paymentSystemBrand
-	 */
-	protected function setPaymentSystemBrand(PaymentSystemBrand $paymentSystemBrand) {
-		$this->paymentSystemBrand = $paymentSystemBrand;
-	}
-
-	/**
 	 * @param Mita $mita
+	 * @return array
 	 */
-	protected function setMita(Mita $mita) {
-		$this->mita = $mita;
-	}
-
-	/**
-	 * @return boolean
-	 */
-	private function getDataSet() {
+	private function getDataSet(
+		PaymentSystemBrand $paymentSystemBrand,
+		Mita $mita
+	) {
 		$matchTable = $this->getMatchTable();
-		if (!isset($matchTable[(string)$this->paymentSystemBrand]
-			[(string)$this->mita]
+		if (!isset($matchTable[(string)$paymentSystemBrand]
+			[(string)$mita]
 		)) {
 			throw new Error("Wrong parameter combination");
 		}
-		$dataSet = $matchTable[(string)$this->paymentSystemBrand]
-			[(string)$this->mita];
+		$dataSet = $matchTable[(string)$paymentSystemBrand]
+			[(string)$mita];
 		return $dataSet;
 	}
 
 	/**
-	 * @return boolean
+	 * @return array
 	 */
 	private function getMatchTable() {
 		return [
 			PaymentSystemBrand::VISA => [
 				Mita::START_RECURRING => [
 					self::CONDITION => '5',
-					self::NEED_RECURRING_DATA => true
+					self::NEED_ADDITIONAL_DATA => true
 				],
 				Mita::INITIATION_BY_MERCHANT => [
 					self::CONDITION => '1',
-					self::NEED_RECURRING_DATA => true
+					self::NEED_ADDITIONAL_DATA => true
 				],
 				Mita::INITIATION_BY_CARDHOLDER => [
 					self::CONDITION => '1',
-					self::NEED_RECURRING_DATA => true
+					self::NEED_ADDITIONAL_DATA => true
 				]
 			],
 			PaymentSystemBrand::MASTERCARD => [
 				Mita::START_RECURRING => [
 					self::CONDITION => '5',
-					self::NEED_RECURRING_DATA => false
+					self::NEED_ADDITIONAL_DATA => false
 				],
 				Mita::INITIATION_BY_MERCHANT => [
 					self::CONDITION => '6',
-					self::NEED_RECURRING_DATA => false
+					self::NEED_ADDITIONAL_DATA => false
 				],
 				Mita::INITIATION_BY_CARDHOLDER => [
 					self::CONDITION => '6',
-					self::NEED_RECURRING_DATA => false
+					self::NEED_ADDITIONAL_DATA => false
 				]
 			],
 			PaymentSystemBrand::MIR => [
 				Mita::START_RECURRING => [
 					self::CONDITION => '5',
-					self::NEED_RECURRING_DATA => true
+					self::NEED_ADDITIONAL_DATA => true
 				],
 				Mita::INITIATION_BY_MERCHANT => [
 					self::CONDITION => '6',
-					self::NEED_RECURRING_DATA => true
+					self::NEED_ADDITIONAL_DATA => true
 				],
 				Mita::INITIATION_BY_CARDHOLDER => [
 					self::CONDITION => '1',
-					self::NEED_RECURRING_DATA => true
+					self::NEED_ADDITIONAL_DATA => true
 				]
 			]
 		];
