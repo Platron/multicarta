d2ed0d38ed11ef38545ea1ed31b7a7bb6fc14974
ryazanov <a.ryazanov@platron.ru>
2018-07-06 13:15:38 +0300

    изменил интерфейс клиента

--- src/pos_xml/Client.php
+++ src/pos_xml/Client.php
@@ -2,76 +2,36 @@
 
 namespace Platron\multicarta\pos_xml;
 
-use SimpleXMLElement;
-
 class Client {
 
 	/**
-	 * @var string
+	 * @param ClientParameters $clientParameters
+	 * @return ClientResult
 	 */
-	private $resultMessage;
+	public function sendRequest(ClientParameters $clientParameters) {
+		$urlWithGetRequest = $clientParameters->getUrlWithGetRequest();
+		$certificatePath = $clientParameters->getCertificatePath();
+		$privateKeyPath = $clientParameters->getPrivateKeyPath();
 
-	/**
-	 * @var string
-	 */
-	private $errorCode;
-
-	/**
-	 * @var string
-	 */
-	private $errorMessage;
-
-	/**
-	 * @param string $url
-	 * @param array $request
-	 * @param string $certificatePath
-	 * @param string $privateKeyPath
-	 * @return SimpleXMLElement
-	 */
-	public function sendRequest(
-		string $url,
-		array $request,
-		string $certificatePath,
-		string $privateKeyPath
-	) {
 		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $url.'?'.http_build_query($request));
+		curl_setopt($curl, CURLOPT_URL, $urlWithGetRequest);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($curl, CURLOPT_SSLCERT, $certificatePath);
 		curl_setopt($curl, CURLOPT_SSLKEY, $privateKeyPath);
 
-		$this->resultMessage = curl_exec($curl);
-		$this->errorCode = curl_errno($curl);
-		$this->errorMessage = curl_error($curl);
+		$returnedData = curl_exec($curl);
+		$errorCode = curl_errno($curl);
+		$errorMessage = curl_error($curl);
+		$result = new ClientResult(
+			$returnedData,
+			$errorCode,
+			$errorMessage
+		);
+
 		curl_close($curl);
-		if ($this->resultMessage !== false) {
-			$response = simplexml_load_string($this->resultMessage);
-			if ($response !== false) {
-				return $response;
-			}
-		}
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getResultMessage() {
-		return $this->resultMessage;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getErrorCode() {
-		return $this->errorCode;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getErrorMessage() {
-		return $this->errorMessage;
+		
+		return $result;
 	}
 }
