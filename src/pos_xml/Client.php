0e3d29146c06f936c30abf6412e0d43a20181285
ryazanov <a.ryazanov@platron.ru>
2018-07-06 13:16:53 +0300

    рефакторинг клиента

--- src/pos_xml/Client.php
+++ src/pos_xml/Client.php
@@ -5,22 +5,49 @@ namespace Platron\multicarta\pos_xml;
 class Client {
 
 	/**
-	 * @param ClientParameters $clientParameters
+	 * @var resource $curl
+	 */
+	private $curl;
+
+	/**
+	 * @param ClientParameters $parameters
 	 * @return ClientResult
 	 */
-	public function sendRequest(ClientParameters $clientParameters) {
-		$urlWithGetRequest = $clientParameters->getUrlWithGetRequest();
-		$certificatePath = $clientParameters->getCertificatePath();
-		$privateKeyPath = $clientParameters->getPrivateKeyPath();
+	public function sendRequest(ClientParameters $parameters) {
+		$this->initializeCurl();
+		$this->setParameters($parameters);
+		$result = $this->getResult();
+		$this->closeCurl();
+		
+		return $result;
+	}
 
+	protected function initializeCurl() {
 		$curl = curl_init();
+		$this->curl = $curl;
+	}
+
+	/**
+	 * @param ClientParameters $parameters
+	 */
+	protected function setParameters(ClientParameters $parameters) {
+		$curl = $this->getCurl();
+		$urlWithGetRequest = $parameters->getUrlWithGetRequest();
+		$certificatePath = $parameters->getCertificatePath();
+		$privateKeyPath = $parameters->getPrivateKeyPath();
 		curl_setopt($curl, CURLOPT_URL, $urlWithGetRequest);
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 		curl_setopt($curl, CURLOPT_SSLCERT, $certificatePath);
 		curl_setopt($curl, CURLOPT_SSLKEY, $privateKeyPath);
+	}
 
+	/**
+	 * @return ClientResult
+	 */
+	protected function getResult() {
+		$curl = $this->getCurl();
 		$returnedData = curl_exec($curl);
 		$errorCode = curl_errno($curl);
 		$errorMessage = curl_error($curl);
@@ -29,9 +56,18 @@ class Client {
 			$errorCode,
 			$errorMessage
 		);
-
-		curl_close($curl);
-		
 		return $result;
+	}
+
+	protected function closeCurl() {
+		$curl = $this->getCurl();
+		curl_close($curl);
+	}
+
+	/**
+	 * @return resource
+	 */
+	protected function getCurl() {
+		return $this->curl;
 	}
 }
